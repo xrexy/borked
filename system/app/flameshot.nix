e673ba3903e38898dcc8e71afef3289c881eea98
xrexy <rkolev@proton.me>
2025-04-05 19:07:14 +0300

    remove flameshot exlucePackages

--- system/app/flameshot.nix
+++ system/app/flameshot.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 
 {
-  anvironment.plasma6.excludePackages = with pkgs; [ spectacle ];
   environment.systemPackages = with pkgs;
     [ (flameshot.override { enableWlrSupport = true; }) ];
 }
