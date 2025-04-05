4cc06b815bf97241b2baead0e91f902c3d7e677d
xrexy <rkolev@proton.me>
2025-04-05 15:02:07 +0300

    add zed editor settings

--- user/app/zed-editor.nix
+++ user/app/zed-editor.nix
@@ -14,6 +14,22 @@
       theme = "Warp One Dark";
       icon_theme = "Bearded Icon Theme";
       vim_mode = true;
+      ui_font_size = 14;
+      buffer_font_size = 12;
+
+      relative_line_numbers = true;
+      show_completions_on_input = false;
+      features = { edit_predicton_provider = "zed"; };
+      edit_predictions = { mode = "subtle"; };
+      project_panel = { dock = "right"; };
+      assistant = {
+        dock = "left";
+        version = "2";
+        default_model = {
+          provider = "zed.dev";
+          model = "claude-3-7-sonnet-latest";
+        };
+      };
       languages = {
         Nix = {
           language_servers = [ "nil" "!nixd" ];
