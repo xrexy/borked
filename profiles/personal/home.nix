b2e14f963e1f21504b1db0b3fcf8f9548f12a9cf
xrexy <rkolev@proton.me>
2025-04-05 18:25:22 +0300

    add minecraft servers dir to xdg

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -8,7 +8,11 @@
   home.homeDirectory = userSettings.homeDirectory;
 
   xdg.userDirs = {
-    extraConfig = { XDG_GAMES_DIR = "${config.home.homeDirectory}/Games"; };
+    extraConfig = {
+      XDG_GAMES_DIR = "${config.home.homeDirectory}/Games";
+      XDG_MINECRAFT_SERVERS_DIR =
+        "${config.home.homeDirectory}/MinecraftServers";
+    };
   };
 
   imports = [ ../work/home.nix ../../user/app/protonup.nix ];
