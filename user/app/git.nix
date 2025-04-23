0273957134487722309b25bea009ceea7d313ef1
xrexy <rkolev@proton.me>
2025-04-23 11:55:52 +0300

    Update git.nix

--- user/app/git.nix
+++ user/app/git.nix
@@ -1,13 +1,20 @@
-{ config, pkgs, userSettings, ... }:
+{ pkgs, userSettings, ... }:
 
 {
   home.packages = [ pkgs.git ];
-  programs.git.enable = true;
-  programs.git.userName = userSettings.name;
-  programs.git.userEmail = userSettings.email;
-  programs.git.extraConfig = {
-    init.defaultBranch = "main";
-    safe.directory =
-      [ userSettings.dotfilesDir (userSettings.dotfilesDir + "/.git") ];
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    userName = userSettings.name;
+    userEmail = userSettings.email;
+    extraConfig = {
+      init.defaultBranch = "main";
+      credential.helper = "${
+          pkgs.git.override { withLibsecret = true; }
+        }/bin/git-credential-libsecret";
+      safe.directory =
+        [ userSettings.dotfilesDir (userSettings.dotfilesDir + "/.git") ];
+      pull.rebase = false;
+    };
   };
 }
